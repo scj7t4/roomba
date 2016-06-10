1a2968cc1210f7b5f4ea6c87f3476372e29ccecd
root <root@localhost.localdomain>
2016-06-10 21:07:50 +0000

    Uhh...

--- src/blep/parse.py
+++ src/blep/parse.py
@@ -13,13 +13,13 @@ def beacon_worker(subp):
     subp.poll()
     while subp.returncode == None:
         line = subp.stdout.readline()
-        print(line)
         parts = line.split(",")
         chunks = map(lambda x: x.split(":"), parts)
         r = {}
         for (key,value) in chunks:
             r[key.strip()] = value.strip()
-        r['date'] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f %z')
+        r['DATE'] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f %z')
+        print(r['MINOR'])
         with beacons_lock:
             beacons[r['MINOR']] = r
         subp.poll()
@@ -37,6 +37,6 @@ if __name__ == "__main__":
             universal_newlines=True)
     worker = threading.Thread(target=beacon_worker, args=(subp,))
     worker.start()
-    app.run(port=6661)
+    app.run(host='0.0.0.0', port=6661)
     subp.terminate()
     worker.join()
