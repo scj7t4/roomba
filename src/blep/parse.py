bef788c5a6faa26a08c4b9fe509c65154c3df522
Stephen Jackson <scj7t4@mst.edu>
2016-06-10 15:43:54 -0500

    Flaskier

--- src/blep/parse.py
+++ src/blep/parse.py
@@ -1,18 +1,42 @@
-import sys
 import json
+import datetime
+import subprocess
+import threading
+
+from flask import Flask, jsonify
 
 beacons = {}
+beacons_lock = threading.Lock()
 
-def worker():
-    global beacons
-    while True:
-        line = sys.stdin.readline()
+def beacon_worker(subp):
+    global beacons, beacons_lock
+    subp.poll()
+    while subp.returncode == None:
+        line = subp.stdout.readline()
+        print(line)
         parts = line.split(",")
         chunks = map(lambda x: x.split(":"), parts)
         r = {}
         for (key,value) in chunks:
             r[key.strip()] = value.strip()
-        beacons[r['MINOR']] = r
-        print(json.dumps(beacons, indent=2))
+        r['date'] = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f %z')
+        with beacons_lock:
+            beacons[r['MINOR']] = r
+        subp.poll()
 
-worker()
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    global beacons, beacons_lock
+    with beacons_lock:
+        return jsonify(**beacons)
+
+if __name__ == "__main__":
+    subp = subprocess.Popen(['/bin/bash', './ibeacon_scan'], stdout=subprocess.PIPE,
+            universal_newlines=True)
+    worker = threading.Thread(target=beacon_worker, args=(subp,))
+    worker.start()
+    app.run(port=6661)
+    subp.terminate()
+    worker.join()
